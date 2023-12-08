f779f1ed34e212d8e5f42fdf05745a52a8567f6e
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2023-12-08 17:45:52 +0100

    added len

--- zad4.pl
+++ zad4.pl
@@ -9,3 +9,6 @@ count(_, [], 0).
 count(X, [X], 1).
 count(X, [X|Tail], N) :- count(X, Tail, P), N is P + 1.
 count(X, [Head|Tail], N) :- dif(X, Head), count(X, Tail, N).
+
+len([], 0).
+len([_|Tail], N) :- len(Tail, M), N is M + 1.
